4683e954632339b90a25ef2d338eb1f6b7ac146b
iago <96024218+iagohenrique2009@users.noreply.github.com>
2022-04-02 12:53:38 -0300

    article-preview-component-master
    frontendmentor.io/ challenge

---
+++ article-preview-component-master (2)/article-preview-component-master/button.js
@@ -0,0 +1,7 @@
+var tooltip = document.querySelector(".iconshr")
+
+document.querySelector(".sharebtn").addEventListener("click", function(){
+    this.classList.toggle("acitve");
+    tooltip.classList.toggle("active"); 
+});
+  
