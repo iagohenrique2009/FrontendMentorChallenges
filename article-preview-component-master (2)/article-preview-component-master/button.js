91ced9aa0fb589411a417b237fc47040dcafa118
iago <96024218+iagohenrique2009@users.noreply.github.com>
2022-04-02 12:56:03 -0300

    Delete article-preview-component-master (2)/article-preview-component-master directory

--- article-preview-component-master (2)/article-preview-component-master/button.js
+++
@@ -1,7 +0,0 @@
-var tooltip = document.querySelector(".iconshr")
-
-document.querySelector(".sharebtn").addEventListener("click", function(){
-    this.classList.toggle("acitve");
-    tooltip.classList.toggle("active"); 
-});
-  
